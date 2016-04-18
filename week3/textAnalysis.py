7dc575bfa1a089a54983d50428bf3b09a6dfbcfc
fesswood <fesswoodwork@gmail.com>
2016-04-18 09:21:49 +0700

    1

--- week3/textAnalysis.py
+++ week3/textAnalysis.py
@@ -18,7 +18,8 @@ print("Loading dataset...")
 t0 = time()
 newsgroups = datasets.fetch_20newsgroups(
     subset='all',
-    categories=['alt.atheism', 'sci.space'])
+    categories=['alt.atheism', 'sci.space'],
+    download_if_missing=True)
 data_samples = newsgroups.data
 print("done in %0.3fs." % (time() - t0))
 '''
@@ -51,8 +52,8 @@ t0 = time()
 tfidf = vectorizer.fit_transform(data_samples)
 print("done in %0.3fs." % (time() - t0))
 
-feature_mapping = vectorizer.get_feature_names()
-print(feature_mapping)
+# feature_mapping = vectorizer.get_feature_names()
+# print(feature_mapping)
 
 '''
 3 Подберите минимальный лучший параметр C из множества [10^-5, 10^-4, ... 10^4, 10^5]
@@ -60,24 +61,57 @@ print(feature_mapping)
 Укажите параметр random_state=241 и для SVM, и для KFold.
 В качестве меры качества используйте долю верных ответов (accuracy).
 '''
-
-grid = {'C': np.power(10.0, np.arange(start=-5.0, stop=5.0, step=1.0))}
-cv = KFold(newsgroups.target.size, n_folds=5, shuffle=True, random_state=241)
-clf = SVC(kernel='linear', random_state=241)
-gs = GridSearchCV(clf, grid, scoring='accuracy', cv=cv)
+skip_grid_calc = True
+if skip_grid_calc:
+    C_optimal = 1.0
+else:
+    grid = {'C': np.power(10.0, np.arange(start=-5.0, stop=6.0, step=1.0))}
+    cv = KFold(newsgroups.target.size, n_folds=5, shuffle=True, random_state=241)
+    clf = SVC(kernel='linear', random_state=241)
+    gs = GridSearchCV(clf, grid, scoring='accuracy', cv=cv)
+    t0 = time()
+    print("fit GridSearchCV...")
+    gs.fit(tfidf, newsgroups.target)
+    print("done in %0.3fs." % (time() - t0))
+    C_optimal = gs.best_estimator_.C
+    print('Best score  ={} , C = {}'.format(gs.best_score_, gs.best_estimator_.C))
+t0 = time()
 
 '''
 Обучите SVM по всей выборке с оптимальным параметром C, найденным на предыдущем шаге.
+Найдите 10 слов с наибольшим абсолютным значением веса (веса хранятся в поле coef_ у svm.SVC).
+Они являются ответом на это задание.
+Укажите эти слова через запятую или пробел, в нижнем регистре, в лексикографическом порядке.
 '''
 t0 = time()
-print("fit GridSearchCV...")
-gs.fit(tfidf, newsgroups.target)
+print("fitting SVC with the best C param...")
+clf1 = SVC(kernel='linear', random_state=241, C=C_optimal)
+clf1.fit(X=tfidf, y=newsgroups.target)
 print("done in %0.3fs." % (time() - t0))
+print("getting coef vector...")
+coef = np.asarray(clf1.coef_.todense()).reshape(-1)
+coef_abs = np.abs(coef)
+#print(top10abs)
+print("----------------")
+debugTest = np.stack((coef_abs, vectorizer.get_feature_names()))
+debugTest = debugTest.transpose()
+#print(debugTest)
+# sort array with regards to nth column
+sortedDebugTest = debugTest[coef_abs.argsort()]
+np.set_printoptions(threshold=np.inf, linewidth=np.inf)  # turn off summarization, line-wrapping
+with open('test.out', 'w') as f:
+    f.write(np.array2string(np.asarray(sortedDebugTest), separator=', '))
+resultWords = sortedDebugTest[-10:][:, 1]
+print(np.sort(resultWords, axis=None))
 
-# for a in gs.grid_scores_:
-# a.mean_validation_score — оценка качества по кросс-валидации
-# a.parameters — значения параметров
-
-
-print("getting max C parameter")
-print(gs.grid_scores_[np.argmax(gs.grid_scores_)])
+# top10arg = np.argsort(top10abs)
+# feature_names = vectorizer.get_feature_names()
+# print(top10arg)
+# top10 = np.argsort(np.abs(np.asarray(clf1.coef_.todense()).reshape(-1)))[-10:]
+# print(top10)
+# print("top 10 keywords")
+# print(top10)
+# result = []
+# for i in top10:
+#     result.append(feature_names[i])
+# print(sorted(result))
